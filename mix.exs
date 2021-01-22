bead38bae5cde4de17b9564a014c8218ae834760
Manfred Wuits <manfred@monochrom.at>
2021-01-22 17:33:52 +0100

    testing bump

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule MavuForm.MixProject do
   use Mix.Project
 
+  @version "0.1.2"
   def project do
     [
       app: :mavu_form,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
